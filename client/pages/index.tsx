baf78a4fd66ba28bda46bb44ad4e803c6b93d7ee
Mouthwatering777 <bluesatellite777@gmail.com>
2018-12-16 11:48:40 -0500

    Front end WIP
    -Made the museum list independently scrollable using flex box.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,29 +8,46 @@ import { MuseumMap } from "../components/search/MuseumMap";
 export default class MuseumSearch extends React.Component {
   render() {
     return (
-      <div className="container-fluid">
+      <div className="container-fluid d-flex flex-column flex-fill">
+        <style>
+          {/* Makes this page non-vertically-scrollable */}
+          {`
+            html, body {
+              margin: 0;
+              height: 100%
+            }
+            #__next {
+              display: flex;
+              flex-flow: column;
+              height: 100%;
+            }
+          `}
+        </style>
         <Head title="Museum Search" />
+        <div className="row flex-shrink-0">
+          <h1 className="col-12">Museum Search</h1>
+        </div>
         <MuseumListQuery>
           {({ loading, error, data, refetch }) => (
-            <div>
-              <div className="row">
-                <h1 className="col-12">Museum Search</h1>
-              </div>
-              <div className="row">
-                <div className="col-md-3">
-                  <div className="card card-body">
-                    <Formik
-                      initialValues={{ query: "" }}
-                      onSubmit={values => refetch(values)}
-                    >
-                      <Form>
-                        <Field
-                          className="form-control form-group"
-                          autoComplete="off"
-                          name="query"
-                        />
-                      </Form>
-                    </Formik>
+            <div className="row flex-fill">
+              <div className="col-md-3">
+                <div className="card card-body h-100">
+                  <Formik
+                    initialValues={{ query: "" }}
+                    onSubmit={values => refetch(values)}
+                  >
+                    <Form>
+                      <Field
+                        className="form-control form-group"
+                        autoComplete="off"
+                        name="query"
+                      />
+                    </Form>
+                  </Formik>
+                  <div
+                    className="h-100 flex-fill"
+                    style={{ overflowY: "scroll" }}
+                  >
                     <MuseumList
                       loading={loading}
                       error={error && error.message}
@@ -38,9 +55,9 @@ export default class MuseumSearch extends React.Component {
                     />
                   </div>
                 </div>
-                <div className="col-md-9">
-                  <MuseumMap />
-                </div>
+              </div>
+              <div className="col-md-9">
+                <MuseumMap />
               </div>
             </div>
           )}

28bc3f2f05f658fcc2187c8531bb8df7a6751024
Mouthwatering777 <bluesatellite777@gmail.com>
2018-12-16 16:25:47 -0500

    Front end WIP
    -Added leaflet and react-leaflet dependencies.
    -Added next-images dependency for compatiblility with Leaflet's CSS.
    -Added MuseumMap workaround to prevent Leaflet from loading during SSR.
    
    -Moved index.tsx CSS into separate variable.

--- client/components/search/MuseumMap.tsx
+++ client/components/search/MuseumMap.tsx
@@ -1,7 +1,39 @@
+import "leaflet/dist/leaflet.css";
 import React from "react";
+import { MapProps, TileLayerProps } from "react-leaflet";
 
 interface MuseumMapProps {}
 
-export const MuseumMap: React.StatelessComponent<MuseumMapProps> = props => (
-  <div>Map goes here</div>
-);
+interface MuseumMapState {
+  /**
+   * The map should only be rendered in a browser. SSR fails if react-leaflet is loaded on the server.
+   */
+  showMap: boolean;
+}
+
+let Map: React.ComponentClass<MapProps>;
+let TileLayer: React.ComponentClass<TileLayerProps>;
+
+export class MuseumMap extends React.Component<MuseumMapProps, MuseumMapState> {
+  state = {
+    showMap: false
+  };
+
+  async componentDidMount() {
+    // Load the actual react-leaflet classes in this method, which is only called in the browser.
+    Map = require("react-leaflet").Map;
+    TileLayer = require("react-leaflet").TileLayer;
+    this.setState({ showMap: true });
+  }
+
+  render() {
+    return Map ? (
+      <Map>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </Map>
+    ) : null;
+  }
+}

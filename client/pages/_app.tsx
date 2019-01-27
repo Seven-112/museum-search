1b440a419a12238c3e91bfe0f2ddb96b23aa53ad
Mouthwatering777 <bluesatellite777@gmail.com>
2019-01-26 19:54:24 -0500

    Front end WIP
    -Added IntrospectionResultData in fragmentTypes.ts to get rid of
     Apollo's console errors.

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,28 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient, {
+  InMemoryCache,
+  IntrospectionFragmentMatcher
+} from "apollo-boost";
 import "isomorphic-fetch";
 import App, { Container } from "next/app";
 import React from "react";
 import { ApolloProvider } from "react-apollo";
-
-const client = new ApolloClient();
+import { fragmentTypes } from "../fragmentTypes";
 
 export default class MyApp extends App {
+  private client: ApolloClient<{}>;
+
+  constructor(props) {
+    super(props);
+
+    // Apollo needs introspection data about fragment types or else it spams the console with errors.
+    // See: validation and accurate fragment
+    const fragmentMatcher = new IntrospectionFragmentMatcher({
+      introspectionQueryResultData: fragmentTypes
+    });
+    const cache = new InMemoryCache({ fragmentMatcher });
+    this.client = new ApolloClient({ cache });
+  }
+
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
 
@@ -21,7 +37,7 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <ApolloProvider client={client}>
+      <ApolloProvider client={this.client}>
         <Container>
           <Component {...pageProps} />
         </Container>
